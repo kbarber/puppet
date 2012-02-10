d7141e7515fe8dbff4f91e0be977f5e81647cdc0
Ken Barber <ken@bob.sh>
2012-02-10 00:23:10 +0000

    (#4561) Fix activerecord object 'InventoryFact' so it serializes/deserializes structured facts

--- lib/puppet/rails/inventory_fact.rb
+++ lib/puppet/rails/inventory_fact.rb
@@ -1,5 +1,18 @@
 require 'puppet/rails/inventory_node'
 
 class Puppet::Rails::InventoryFact < ::ActiveRecord::Base
+  include Puppet::Util::ReferenceSerializer
+  extend Puppet::Util::ReferenceSerializer
+
   belongs_to :node, :class_name => "Puppet::Rails::InventoryNode"
+
+  def value
+    # Because rails doesn't deserialize for you
+    unserialize_value(self[:value])
+  end
+
+  def value=(val)
+    # Rails will normally serialize for you, but this ensures it
+    self[:value] = serialize_value(val)
+  end
 end
