7be58e20b449343e7ab9033e7245ed1a9ce4f4c2
Ken Barber <ken@bob.sh>
2012-02-19 19:22:14 +0000

    (#4561) Add feature for facter, including version checking

---
+++ lib/puppet/feature/facter.rb
@@ -0,0 +1,27 @@
+require 'puppet/util/feature'
+require 'semver'
+
+# See if Facter is available, and check revision
+Puppet.features.add(:facter) do
+  begin
+    require 'facter'
+  rescue LoadError => detail
+    require 'rubygems'
+    require 'facter'
+  end
+
+  if ! (defined?(::Facter) and defined?(::Facter.version))
+    Puppet.err "Cannot find Facter class or version declaration"
+    false
+  else
+    facter_version = ::SemVer.new(::Facter.version)
+    required_version = ::SemVer.new("2.0.0")
+    if facter_version >= required_version
+      true
+    else
+      Puppet.err "Facter 2.0.0 or greater required for Puppet to operate"
+      false
+    end
+  end
+end
+
