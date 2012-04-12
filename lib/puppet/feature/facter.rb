9aa742b4fcb81cc085ae195c7a36409631ac4e0e
Ken Barber <ken@bob.sh>
2012-04-12 13:40:46 +0200

    (#4561) Cleanup facter feature

--- lib/puppet/feature/facter.rb
+++ lib/puppet/feature/facter.rb
@@ -3,23 +3,31 @@ require 'semver'
 
 # See if Facter is available, and check revision
 Puppet.features.add(:facter) do
+  required_facter = "2.0.0"
+
   begin
     require 'facter'
-  rescue LoadError => detail
+  rescue LoadError
     require 'rubygems'
     require 'facter'
   end
 
-  if ! (defined?(::Facter) and defined?(::Facter.version))
-    Puppet.err "Cannot find Facter class or version declaration"
+  if ! defined?(::Facter)
+    Puppet::Error "Cannot find Facter class. Facter may not be installed, " +
+      "or not be in your RUBYLIB."
     false
+  elsif ! defined?(::Facter.version)
+    Puppet::Error "Cannot find Facter version declaration. Your " +
+      "installation of Facter may be invalid, very old or this may be a bug."
   else
     facter_version = ::SemVer.new(::Facter.version)
-    required_version = ::SemVer.new("2.0.0")
+    required_version = ::SemVer.new(required_facter)
     if facter_version >= required_version
       true
     else
-      raise Puppet::Error, "Facter 2.0.0 or greater required for Puppet to operate"
+      raise Puppet::Error, "Found Facter version #{::Facter.version} " +
+        "however version #{required_facter} (or greater) is required for " +
+        "Puppet to operate"
     end
   end
 end
